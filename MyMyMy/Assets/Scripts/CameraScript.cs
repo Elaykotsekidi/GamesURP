6832b5585c8bde774a8aa42e85ce34260896be25
Elaykotsekidi <elaykotsekidi@gmail.com>
2024-02-13 18:44:23 +0300

    Stash
    Stash

--- MyMyMy/Assets/Scripts/CameraScript.cs
+++ MyMyMy/Assets/Scripts/CameraScript.cs
@@ -1,21 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CameraScript : MonoBehaviour
 {
 
     [SerializeField] GameObject _FollowedObject;
-    [SerializeField] Camera _Camera;
+    [SerializeField] GameObject _Camera;
+    [SerializeField] float _CameraRotateSpeed = 0f;
+    [SerializeField] float _CameraMoveSpeed = 5.0f;
+    Transform _CameraTransform;
 
-    void Start()
+
+    private void Start()
     {
+    _CameraTransform = GetComponent<Transform>();
         
     }
 
-
     void Update()
     {
-        _Camera.transform.LookAt(_FollowedObject.transform.position);
+        CameraRotate();
+        CameraMove();
     }
+
+    void CameraRotate()
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            _Camera.transform.Rotate(0f, 0.2f, 0f * Time.deltaTime * _CameraRotateSpeed, Space.World);
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            _Camera.transform.Rotate(0f, -0.2f, 0f * Time.deltaTime * _CameraRotateSpeed, Space.World);
+        }
+    }
+    void CameraMove()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            
+            _Camera.transform.Translate(0f, 0f, 1f * Time.deltaTime * _CameraMoveSpeed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            _Camera.transform.Translate(0f, 0f, -1f * Time.deltaTime * _CameraMoveSpeed);
+        }
+    }
+    
+    
 }
